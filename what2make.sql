e7fc81255fde140d9f5780299e61634bc8c40a86
Julian Velez <velezj425@users.noreply.github.com>
2017-02-25 23:26:32 -0500

    beginnings of my site database

---
+++ what2make.sql
@@ -0,0 +1,40 @@
+# Julian Velez
+# This is a database for my reverse recipe look-up "What to Make"
+
+CREATE DATABASE what2make;
+
+# Table for User data
+CREATE TABLE Users (
+    userID varchar(255),
+    fName varchar(255),
+    lName varchar(255),
+    password varchar(255),
+    email varchar(255),
+    PRIMARY KEY (userID)
+);
+
+# Table for Site data 
+CREATE TABLE Sites (
+    siteID varchar(255),
+    siteName varchar(255)
+);
+
+# Table for Recipe data
+CREATE TABLE Recipes (
+    recipeID varchar(255),
+    title varchar(255),
+    siteID varchar(255),
+    url varchar(255),
+    PRIMARY KEY (recipeID),
+    FOREIGN KEY (siteID) REFERENCES Sites(siteID)
+);
+
+# Table linking Users to their saved Recipes
+CREATE TABLE UserSavedRecipes (
+    userID varchar(255),
+    recipeID varchar(255),
+    PRIMARY KEY (userID, recipeID),
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+    FOREIGN KEY (recipeID) REFERENCES Recipes(recipeID)
+);
+
