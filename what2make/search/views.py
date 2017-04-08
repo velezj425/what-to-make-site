f7c09f2d7ec8cfc8bd09b16eaa6b8975d473a2ea
Julian Velez <jvelez42592@gmail.com>
2017-04-07 21:54:45 -0400

    Created landing page

--- what2make/search/views.py
+++ what2make/search/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader
 
 def home(request):
-    return HttpResponse("This is a sign-in page!")
+    template = loader.get_template('search/signin.html')
+    return HttpResponse(template.render(request))
