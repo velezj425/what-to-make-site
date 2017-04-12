dbc96b3262a36ff5ea8c9d0c472e2677a3eb91a1
Julian Velez <jvelez42592@gmail.com>
2017-04-11 22:41:47 -0400

    "Prepping for profile view"

--- what2make/search/views.py
+++ what2make/search/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is the signin page.")
+
+def profile(request, profile_id):
+    return HttpResponse("This is the profile for user %s." %profile_id)
