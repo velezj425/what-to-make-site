3a03f45b9dccc51e13d4c6d690e37713d6a43c48
Julian Velez <jvelez42592@gmail.com>
2017-04-11 22:22:39 -0400

    Updated models some more..

--- what2make/search/admin.py
+++ what2make/search/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Profile)
+admin.site.register(Recipe)
+admin.site.register(Ing_Type)
+admin.site.register(Ingredient)
+
--- what2make/search/models.py
+++ what2make/search/models.py
@@ -6,11 +6,17 @@ from django.db import models
 class Ing_Type(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 # ingredient model
 class Ingredient(models.Model):
     name = models.CharField(max_length=20)
     ing_type = models.ForeignKey(Ing_Type, on_delete=models.CASCADE)
     cost = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
 
 # recipes model
 class Recipe(models.Model):
@@ -18,11 +24,17 @@ class Recipe(models.Model):
     url = models.URLField()
     ingredients = models.ManyToManyField(Ingredient)
 
+    def __str__(self):
+        return self.title + " (" + self.url + ")"
+
 # user profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    saved = models.ManyToManyField(Recipe)
-    blocked = models.ManyToManyField(Ingredient)
+    saved = models.ManyToManyField(Recipe, blank=True)
+    blocked = models.ManyToManyField(Ingredient, blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 # rating model
 class Rating(models.Model):
