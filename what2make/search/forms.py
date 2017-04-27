af0b86356cf2e7f983023eb9ed446aea433b8452
Julian Velez <jvelez42592@gmail.com>
2017-04-27 00:29:58 -0400

    Worked on More Forms and added logout view

--- what2make/search/forms.py
+++ what2make/search/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', widget=forms.TextInput())
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
+
+class NewUserForm(forms.Form):
+    username = forms.CharField(label='Username', widget=forms.TextInput())
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput())
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput())
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    recheck_pass = forms.CharField(label='Re-Enter Password', widget=forms.PasswordInput())
--- what2make/search/views.py
+++ what2make/search/views.py
@@ -1,21 +1,32 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, NewUserForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    #template = loader.get_template('search/index.html')
-    form = LoginForm(request.POST)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/signup/')
+    form = LoginForm(request.POST or None)
     return render(request,'search/index.html',{'form':form})
 
+@login_required
 def profile(request, profile_id):
     return HttpResponse("This is the profile for user %s." %profile_id)
 
+@login_required
 def query(request, profile_id):
     return HttpResponse("This is the search page.")
 
+@login_required
 def result(request, profile_id):
     return HttpResponse("This is the result page.")
 
 def signup(request):
-    return HttpResponse("This is the signup page.")
+    form = NewUserForm(request.POST or None)
+    return render(request,'search/signup.html',{'form':form})
+
+def logout_view(request):
+    logout(request)
